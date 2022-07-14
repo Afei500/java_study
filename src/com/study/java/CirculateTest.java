fab98e07745c8be13edf7423e52569c830238ef0
Afei500 <1179705488@qq.com>
2022-07-15 01:49:35 +0800

    7-15commit

---
+++ src/com/study/java/CirculateTest.java
@@ -0,0 +1,73 @@
+package com.study.java;
+
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: zhao
+ * Date: 2022-07-14
+ * Time: 16:57
+ */
+public class CirculateTest {
+    public static void main(String[] args) {
+
+/*
+//        第三题
+        for (int i = 1; i <= 150; i++) {
+            System.out.print(i);
+            if (i%3==0){
+                System.out.print("foo");
+            }
+            if(i%5==0){
+                System.out.print("biz");
+            }
+            if (i%7==0){
+                System.out.print("baz");
+            }
+            System.out.println();
+        }
+
+//        第四题最大公约数最小公倍数
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个正数");
+        int a = sc.nextInt();
+        System.out.println("请输入一个正数");
+        int b = sc.nextInt();
+        int num=0;
+        if(a>=b){
+            num=a;
+        }else{
+            num=b;
+        }
+        for (;;num++){
+           if((num%a==0)&&(num%b==0)){
+               break;
+           }
+        }
+        System.out.println(num);
+
+
+//        第五题
+
+        for (int i = 100; i <1000; i++) {
+            if ((i%10)*(i%10)*(i%10)+(i/100)*(i/100)*(i/100)+(i%100/10)*(i%100/10)*(i%100/10)==i){
+                System.out.println(i);
+            }
+        }
+
+
+//        第七题
+        for (int i = 1; i <=5; i++) {
+            for (int j = 0; j <i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+ */
+//        第八题
+
+
+    }
+
+}
---
+++ src/com/study/java/SwitchCaseTest.java
@@ -0,0 +1,81 @@
+package com.study.java;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: zhao
+ * Date: 2022-07-14
+ * Time: 14:35
+ */
+public class SwitchCaseTest {
+    public static void main(String[] args) {
+          /*
+        int num=3;
+//        double num=1; double 类型报错，switch后面必须是整型，case后面是常量表达式
+
+        switch (num){
+
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            default:
+                System.out.println("没有");
+                break;
+        }
+
+
+
+
+//    第一题
+        System.out.println("请输入一个成绩");
+        Scanner sc=new Scanner(System.in);
+        int score=sc.nextInt();
+        switch (score/60){
+            case 0:
+                System.out.println("不合格");
+                break;
+            case 1:
+                System.out.println("合格");
+                break;
+    }
+//        第二题
+        Scanner sc=new Scanner(System.in);
+        System.out.println("请输入月份");
+        int month=sc.nextInt();
+        System.out.println("请输入日期");
+        int day=sc.nextInt();
+        int sum=0;
+        switch (month){
+            case 12:
+                sum+=30;
+            case 11:
+                sum+=30;
+            case 10:
+                sum+=30;
+            case 9:
+                sum+=30;
+            case 8:
+                sum+=30;
+            case 7:
+                sum+=30;
+            case 6:
+                sum+=30;
+            case 5:
+                sum+=30;
+            case 4:
+                sum+=30;
+            case 3:
+                sum+=30;
+            case 2:
+                sum+=30;
+            case 1:
+                sum+=day;
+        }
+        System.out.println(sum);
+        */
+
+    }
+}
