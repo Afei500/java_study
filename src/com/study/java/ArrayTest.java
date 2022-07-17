e08dc832b44e5f63c3a2b20636aa9071419bc2e1
Afei500 <1179705488@qq.com>
2022-07-18 00:21:42 +0800

    7-17commit

--- src/com/study/java/ArrayTest.java
+++ src/com/study/java/ArrayTest.java
@@ -1,5 +1,7 @@
 package com.study.java;
 
+import java.util.Scanner;
+
 /**
  * Created with IntelliJ IDEA.
  * Description:
@@ -9,6 +11,60 @@ package com.study.java;
  */
 public class ArrayTest {
     public static void main(String[] args) {
+        /*
+//        第十二题
+//        根据学生人数定义数组长度
+        Scanner sc = new Scanner(System.in);
+        System.out.print("请输入学生人数:");
+        int i = sc.nextInt();
+        int[] i1 = new int[i];
+
+        //将学生成绩放进数组
+        System.out.println("请输入"+i+"个学生的成绩");
+        for (int a = 0; a < i; a++) {
+            i1[a]=sc.nextInt();
+        }
+      //遍历数组打擂台找最大值
+        int max=i1[0];
+        for (int b = 0; b < i1.length; b++) {
+            max = max >= i1[b] ? max : i1[b];
+        }
+        System.out.println("最大成绩是:"+max);
+//            判断成绩等级
+        for (int b = 0; b < i1.length; b++) {
+            char grade='A';
+            if(i1[b] >= (max-10)){
+                grade='A';
+            }else if (i1[b] >= (max-20)){
+                grade='B';
+
+            }else if (i1[b] >= (max-30)){
+                grade='C';
+            }else{
+
+                grade='D';
+            }
+
+
+
+            System.out.println("student "+b+" score is "+i1[b]+" grade is "+grade);
+        }
+
+
+//        二维数组的遍历
+        int[][] arr=new int[][]{{1,2,3},{4,5},{6,7,8}};
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j]+"  ");
+            }
+            System.out.println();
+        }
+
+
+         */
+
+
+
 
     }
 }
